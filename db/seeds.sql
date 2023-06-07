e508349f700b13a93e7cd83a40182044fea21bcb
C12 <mcpartlandpatrick@gmail.com>
2023-06-07 10:19:01 -0400

    added challenge readme, updated seeds file

--- db/seeds.sql
+++ db/seeds.sql
@@ -4,5 +4,21 @@ VALUES(001, "Sales"),
       (003, "Finance"),
       (004, "Legal");
 
-INSERT INTO role (id, title, salary)
-VALUES()
+INSERT INTO role (title, salary, department_id)
+VALUES("Sales Lead", 100000, 1)
+("Salesperson" 80000, 1)
+("Lead Engineer" 150000, 2)
+("Software Engineer" 120000, 2)
+("Account Manager" 160000, 3)
+("Accountant" 125000, 3)
+("Legal Team Lead" 250000, 4)
+("Lawyer" 190000, 4)
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES("John", "Doe",)
+      ("Mike", "Chan",)
+      ("Ashley", "Rodriguez",)
+      ("Kevin", "Tupik",)
+      ("Kunal", "Singh",)
+      ("Malia", "Brown",)
+      ("Sarah", "Lourd",)
+      ("Tom", "Allen",)
